c927eb9d0ac52086b5b0441a399886be20e34297
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-02-18 21:07:16 +0900

    Refactor: 스타일 코드 분리

--- src/components/common/aiSubmitButton/AISubmitBtn.jsx
+++ src/components/common/aiSubmitButton/AISubmitBtn.jsx
@@ -1,25 +1,4 @@
-import styled from 'styled-components';
-import ArrowRight from '../../../assets/svgs/ic_arrow_right.svg?react';
-
-const StyledButton = styled.button`
-  display: flex;
-  width: 36px;
-  height: 36px;
-  padding: 8px;
-  align-items: center;
-  border-radius: 50%;
-  background-color: ${({ theme }) => theme.colors.red};
-
-  &:hover {
-    background-color: ${({ theme }) => theme.colors.red_hover};
-  }
-`;
-
-const ArrowIcon = styled(ArrowRight)`
-  width: 20px;
-  height: 20px;
-  color: white;
-`;
+import { StyledButton, ArrowIcon } from './index.style';
 
 const Button = ({ onClick }) => {
   return (
