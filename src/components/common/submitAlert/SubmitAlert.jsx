50f2a6c42cee8508e21f1eecd0f61da74b6530a3
kminsoo0110 <kminsoo0110@naver.com>
2025-02-21 14:36:00 +0900

    메인페이지로 돌아가는 로직 구현

--- src/components/common/submitAlert/SubmitAlert.jsx
+++ src/components/common/submitAlert/SubmitAlert.jsx
@@ -1,11 +1,17 @@
+import { useNavigate } from "react-router-dom";
 import { Title, StyledButton, SytledScreen} from "./index.style";
 
-const SubmitAlert = (onclick) => {
+const SubmitAlert = () => {
+    const navigate = useNavigate();
     return(
         <SytledScreen>
             <Title>메일이 정상적으로 전송되었습니다!</Title>
-            <StyledButton onclick = {onclick}>
-                메인 페이지로 돌아가기
+            <StyledButton 
+                onClick = {() => { 
+                    navigate('/');
+                }}
+            >
+            메인 페이지로 돌아가기
             </StyledButton>
         </SytledScreen>
     );
