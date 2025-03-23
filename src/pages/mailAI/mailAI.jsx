178675f5526105b5dcbf54cbb8bcaf7a3b3147fe
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-03-23 21:38:45 +0900

    feat: 페이지 초기 테스트

---
+++ src/pages/mailAI/mailAI.jsx
@@ -0,0 +1,5 @@
+const mailAI = () => {
+  return <div>AI 작성 페이지</div>;
+};
+
+export default mailAI;
---
+++ src/pages/mailTemplate/mailTemplate.jsx
@@ -0,0 +1,5 @@
+const mailTemplate = () => {
+  return <div>템플릿 작성 페이지</div>;
+};
+
+export default mailTemplate;
