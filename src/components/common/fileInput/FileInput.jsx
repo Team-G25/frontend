e3443ffabf4883712d7da9908af19df3b0142486
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-03-24 03:18:24 +0900

    feat: 첨부한 파일 제목 구현

--- src/components/common/fileInput/FileInput.jsx
+++ src/components/common/fileInput/FileInput.jsx
@@ -1,19 +1,48 @@
-import { StyledButton, ButtonContainer, StyledFrame, File, Photo } from "./index.style";
+import { useRef, useState } from 'react';
+import {
+  StyledButton,
+  ButtonContainer,
+  StyledFrame,
+  File,
+  Photo,
+  FileName,
+} from './index.style';
 
-const FileInput = ({width, onclick}) => {
-    return (
-        <StyledFrame width={width}>
-            <ButtonContainer>
-                <StyledButton onclick={onclick}>
-                    <Photo />
-                </StyledButton>
-                <StyledButton onclick={onclick}>
-                    <File />
-                </StyledButton>
-            </ButtonContainer>
-        </StyledFrame>
+const FileInput = ({ width }) => {
+  const fileInputRef = useRef(null);
+  const [fileName, setFileName] = useState('');
 
-    );
+  const handleClick = () => {
+    fileInputRef.current?.click();
+  };
+
+  const handleFileChange = (event) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const selectedFile = files[0];
+      setFileName(selectedFile.name);
+    }
+  };
+
+  return (
+    <StyledFrame width={width}>
+      <input
+        type="file"
+        ref={fileInputRef}
+        onChange={handleFileChange}
+        style={{ display: 'none' }}
+      />
+      <ButtonContainer>
+        <StyledButton onClick={handleClick}>
+          <Photo />
+        </StyledButton>
+        <StyledButton onClick={handleClick}>
+          <File />
+        </StyledButton>
+      </ButtonContainer>
+      <FileName>{fileName}</FileName>
+    </StyledFrame>
+  );
 };
 
-export default FileInput
+export default FileInput;
--- src/components/common/fileInput/index.style.js
+++ src/components/common/fileInput/index.style.js
@@ -1,40 +1,52 @@
-import styled from "styled-components";
-import theme from "@/styles/theme";
-import FileIcon from '../../../assets/svgs/ic_file.svg?react'
-import PhotoIcon from '../../../assets/svgs/ic_photo.svg?react'
-
+import styled from 'styled-components';
+import theme from '@/styles/theme';
+import FileIcon from '../../../assets/svgs/ic_file.svg?react';
+import PhotoIcon from '../../../assets/svgs/ic_photo.svg?react';
 
 export const StyledFrame = styled.div.attrs((props) => ({
-    style: {
-        width: props.width || "612px", // 기본값 612px
-    },
+  style: {
+    width: props.width || '612px', // 기본값 612px
+  },
 }))`
-    display: flex;
-    height: 74px;
-    padding: 17px 0 17px 32px;
-    border-radius: 60px;
-    background-color: ${theme.colors.white1};
+  display: flex;
+  align-items: center;
+  height: 74px;
+  padding: 17px 0 17px 32px;
+  border-radius: 60px;
+  background-color: ${theme.colors.white1};
 `;
 
 export const ButtonContainer = styled.div`
-    display: inline-flex;
-    align-items: center;
-    gap: 24px;
-    position: relative;
+  display: inline-flex;
+  align-items: center;
+  gap: 24px;
+  position: relative;
 `;
 
 export const StyledButton = styled.button`
-    height: 40px;
-    width: 40px;
-    position: relative;
+  height: 40px;
+  width: 40px;
+  position: relative;
 `;
 
 export const Photo = styled(PhotoIcon)`
-    width: 35px;
-    height: 30px;
+  width: 35px;
+  height: 30px;
 `;
 
 export const File = styled(FileIcon)`
-    width: 35px;
-    height: 30px;
+  width: 35px;
+  height: 30px;
+`;
+
+export const FileName = styled.span`
+  margin-left: 16px;
+  font-family: ${theme.font.family.popup};
+  font-weight: ${theme.font.weight.medium};
+  font-size: ${theme.font.size.medium};
+  color: ${theme.colors.blue};
+  max-width: 600px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
