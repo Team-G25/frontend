aeab84f326ce2e72ee34068668275b783f3abd12
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-03-24 22:05:19 +0900

    refactor: 대상 선택 버튼의 선택 상태를 상위 컴포넌트에서 제어하도록 변경

--- src/components/common/targetButton/TargetBtn.jsx
+++ src/components/common/targetButton/TargetBtn.jsx
@@ -1,22 +1,19 @@
-import { useState } from 'react';
 import { Title, ButtonContainer, StyledButton } from './index.style';
 
-const TargetBtn = () => {
-  const [selected, setSelected] = useState(null);
-
+const TargetBtn = ({ selected, onSelect }) => {
   return (
     <>
-      <Title>직업</Title>
+      <Title>대상</Title>
       <ButtonContainer>
         <StyledButton
           $isSelected={selected === '학생'}
-          onClick={() => setSelected('학생')}
+          onClick={() => onSelect('학생')}
         >
           학생
         </StyledButton>
         <StyledButton
           $isSelected={selected === '직장인'}
-          onClick={() => setSelected('직장인')}
+          onClick={() => onSelect('직장인')}
         >
           직장인
         </StyledButton>
