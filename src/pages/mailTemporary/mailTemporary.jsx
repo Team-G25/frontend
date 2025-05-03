da7680dff87de396901f43ed67a1cb03fb12020c
kminsoo0110 <kminsoo0110@naver.com>
2025-05-03 17:52:44 +0900

    [Feat] 삭제버튼 누르면 MODAL창 등장.

--- src/pages/mailTemporary/mailTemporary.jsx
+++ src/pages/mailTemporary/mailTemporary.jsx
@@ -7,12 +7,14 @@ import Bottom1 from "./components/common/bottom/bottom1";
 import MailButton from "./components/common/mailButton/mailButton";
 import Sidebar from "@/components/common/sideBar/SideBar";
 import MailEditor from "../mailTemplate/components/mailEditor/MailEditor";
+import DeleteModal from "./components/common/deletePopuUp/deletePopUp";
 
 const MailTemporary = () => {
     const [drafts, setDrafts] = useState([]); //현재 페이지에서 보일 데이터
     const [editDraft, setEditDraft] = useState(null); //편집중 상태
     const [isLoading, setIsLoading] = useState(false); 
-    
+    const [showModal, setShowModal] = useState(false); //삭제 버튼 누를시 모달띄움
+
     //전체 메일 갖고오기
     useEffect(() =>{
         const loadAllDrafts = async () => {
@@ -31,15 +33,23 @@ const MailTemporary = () => {
         loadAllDrafts();
     }, []);
     
+    //메일 전체 삭제 누를시 모달창 띄우기
+    const handleDelete = async () =>{
+        setShowModal(true);
+        console.log("모달창 띄웁니다.")
+    };
+
     //메일 전체 삭제
-    const handleDelete = async (draftID) => {
+    const handleConfirmDelete = async (draftID) => {
         try {
             await deleteAllDrafts(draftID);
             setDrafts([]);
             console.log("모든 메일 삭제 완료");
         } catch (error) {
             console.error("전체 삭제 오류:", error);
-        }
+        } finally {
+            setShowModal(false);
+        };
     };
 
     //임시 메일 편집 상태설정
@@ -51,6 +61,12 @@ const MailTemporary = () => {
     const handleCancelEdit = () => {
         setEditDraft(null);
     }
+
+    //모달 닫기
+    const handleClose = () => {
+        setShowModal(false);
+        console.log("모달 닫습니다.");
+    } 
 
     return (
         <PageContainer>
@@ -87,6 +103,14 @@ const MailTemporary = () => {
                         ))
                     )}        
                 </>            
+            )}
+
+            {/* MODAL screen */}
+            {showModal && (
+                <DeleteModal
+                    onClose={handleClose}
+                    onConfirm={handleConfirmDelete}
+                />
             )}
             </MainContent>
         </PageContainer>
