f4a1fb473713c0e1e3916e345861eafaa8b8966d
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-05-09 11:06:56 +0900

    fix

--- src/pages/auth/signup.jsx
+++ src/pages/auth/signup.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import TermsAgreement from './components/menu/TermsAgreement';
 import IdentityVerification from './components/menu/IdentityVerification';
 import ProfileSetup from './components/menu/ProfileSetup';
-import Sidebar from './components/sidebar/Sidebar';
+// import Sidebar from './components/sidebar/Sidebar';
 
 const SignUpPage = () => {
   const [step, setStep] = useState('terms');
@@ -33,7 +33,7 @@ const SignUpPage = () => {
 
   return (
     <PageWrapper>
-      <Sidebar step={step} setStep={setStep} />
+      {/* <Sidebar step={step} setStep={setStep} /> */}
       <ContentWrapper>{renderStepComponent()}</ContentWrapper>
     </PageWrapper>
   );
