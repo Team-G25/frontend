dad0b1fa4f6d20a282d31f0e47937eb24291242a
syddl0 <137189866+shroqkf@users.noreply.github.com>
2025-05-09 11:24:21 +0900

    Fix: LoginSidebar 생성

---
+++ src/pages/auth/components/sidebar/LoginSidebar.jsx
@@ -0,0 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import {
+  SidebarContainer,
+  Logo,
+  MenuList,
+  MenuItem,
+  LogoImage,
+} from './index.style';
+
+const LoginSidebar = ({ step, setStep }) => {
+  const navigate = useNavigate();
+
+  return (
+    <SidebarContainer>
+      <h1>회원가입</h1>
+      <MenuList>
+        <MenuItem active={step === 'terms'} onClick={() => setStep('terms')}>
+          약관 동의
+        </MenuItem>
+        <MenuItem
+          active={step === 'identity'}
+          onClick={() => setStep('identity')}
+        >
+          본인 인증
+        </MenuItem>
+        <MenuItem
+          active={step === 'profile'}
+          onClick={() => setStep('profile')}
+        >
+          프로필 설정
+        </MenuItem>
+      </MenuList>
+      <Logo onClick={() => navigate('/')}>
+        <LogoImage />
+      </Logo>
+    </SidebarContainer>
+  );
+};
+
+export default LoginSidebar;
